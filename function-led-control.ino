41e115fb094d4772cbe1c034c4ac6dd1c9fc45b4
Jeremy Keen <jeremykeen@gmail.com>
2015-09-23 09:17:43 -0400

    updated with web build version

--- function-led-control.ino
+++ function-led-control.ino
@@ -1,7 +1,7 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "InternetButton/InternetButton.h"
-#include <iostream>
-#include <string>
+#include <stdlib.h>     /* atoi */
+
 
 // -----------------------------------
 // Controlling LEDs over the Internet
@@ -13,7 +13,14 @@
 
 int led1 = D0;
 int led2 = D7;
+int i;
+float howMany;
+int red;
+int green;
+int blue;
 int utilPercentage;
+int swVersion = 2;
+
 
 // Last time, we only needed to declare pins in the setup function.
 // This time, we are also going to register our Spark function
@@ -24,11 +31,14 @@ bool rainbow_mode = false;
 void setup()
 {
 
- b.begin();
- b.allLedsOn(0,128,0);
- // We are also going to declare a Spark.function so that we can turn the LED on and off from the cloud.
- Spark.function("led",ledToggle);
- // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
+   b.begin();
+   b.allLedsOn(0,128,0);
+   // We are also going to declare a Spark.function so that we can turn the LED on and off from the cloud.
+   Spark.function("led",ledToggle);
+   // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
+   Spark.variable("howMany", &howMany, INT);
+   Spark.variable("utilPercentage", &utilPercentage, INT);
+   Spark.variable("swVersion", &swVersion, INT);
 
 }
 
@@ -39,7 +49,7 @@ void setup()
 
 void loop()
 {
- // Nothing to do here
+   // Nothing to do here
 }
 
 // We're going to have a super cool function now that gets called when a matching API request is sent
@@ -47,41 +57,50 @@ void loop()
 
 
 int ledToggle(String command) {
-  /* Spark.functions always take a string as an argument and return an integer.
-  Since we can pass a string, it means that we can give the program commands on how the function should be used.
-  In this case, telling the function "on" will turn the LED on and telling it "off" will turn the LED off.
-  Then, the function returns a value to us to let us know what happened.
-  In this case, it will return 1 for the LEDs turning on, 0 for the LEDs turning off,
-  and -1 if we received a totally bogus command that didn't do anything to the LEDs.
-  */
+    /* Spark.functions always take a string as an argument and return an integer.
+    Since we can pass a string, it means that we can give the program commands on how the function should be used.
+    In this case, telling the function "on" will turn the LED on and telling it "off" will turn the LED off.
+    Then, the function returns a value to us to let us know what happened.
+    In this case, it will return 1 for the LEDs turning on, 0 for the LEDs turning off,
+    and -1 if we received a totally bogus command that didn't do anything to the LEDs.
+    */
+    b.allLedsOff();
 
-utilPercentage = std::stoi(command);
-  if (command>="70") {
-      b.allLedsOn(255,255,0);
-      return 1;
-  }
-  else if (command=="green") {
+    utilPercentage = atoi (command);
+    howMany = ((utilPercentage/100) * 12);
+    //if 70 all LEDs are yellow
+    if (utilPercentage>=70) {
+        int red = 255;
+        int green = 255;
+        int blue = 0;
+        for(int i = 1; i <= howMany; i++){
+        b.ledOn(i, red, green, blue);
+        }
+        // b.allLedsOn(255,255,0); //yellow
+        return 1;
+    }
+    else if (command=="green") {
         b.allLedsOn(0,128,0);
-      return 1;
-  }
-  else if (command=="red") {
-      b.allLedsOn(255,0,0);
-      return 1;
-  }
-  else if (command=="rainbow") {
-      //Enable rainbox mode
-      b.rainbow(20);
-      rainbow_mode = true;
-      //delay(10000);
-      //b.allLedsOff();
-      return 1;
-  }
-  else if (command=="off") {
-      b.allLedsOff();
-      rainbow_mode = false;
-      return 0;
-  }
-  else {
-      return -1;
-  }
+        return 2;
+    }
+    else if (command=="red") {
+        b.allLedsOn(255,0,0);
+        return 3;
+    }
+    else if (command=="rainbow") {
+        //Enable rainbox mode
+        b.rainbow(20);
+        rainbow_mode = true;
+        //delay(10000);
+        //b.allLedsOff();
+        return 4;
+    }
+    else if (command=="off") {
+        b.allLedsOff();
+        rainbow_mode = false;
+        return 0;
+    }
+    else {
+        return -1;
+    }
 }
