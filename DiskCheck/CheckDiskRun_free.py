7960d2e43a44cd9b4dc7d4e69a41a0a6f6cf89a4
BowersRC <robert.bowers@emc.com>
2015-09-24 11:29:10 -0400

    Updated with Arg and Free

--- DiskCheck/CheckDiskRun_free.py
+++ DiskCheck/CheckDiskRun_free.py
@@ -31,9 +31,32 @@ ds = DiskSpace()
 perc_arg = len(sys.argv) > 1 and sys.argv[1] or None
 percent_limit = perc_arg or 20
 
+def send_request():
+# call function
+# POST https://api.particle.io/v1/devices/330048000547343233323032/led
+
+    try:
+        response = requests.post(
+            url="https://api.particle.io/v1/devices/330048000547343233323032/led",
+            headers={
+                "Authorization": "Bearer ad84d0f05262309428b7fa8595c360a33609537b",
+                "Content-Type": "application/json",
+            },
+            data=json.dumps({
+                "led": ds.has_free_space(percent_limit)
+            })
+        )
+        print('Response HTTP Status Code: {status_code}'.format(
+            status_code=response.status_code))
+        print('Response HTTP Response Body: {content}'.format(
+            content=response.content))
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+send_request ()
+
 if not ds.has_free_space(percent_limit):
     print "Running low on disk space. %s%% remaining "\
-       "(Warning Threshold: %s%%)" % (ds.percent_free(), percent_limit)
+       "(Warning Threshold: %s%%)" % (ds.percent_free(), percent_limit)	
     print "Total: %s" % ds.humanize_bytes(ds.bytes_capacity(), 1000)
     print "Used:  %s" % ds.humanize_bytes(ds.bytes_used(), 1000)
     print "Avail: %s" % ds.humanize_bytes(ds.bytes_free(), 1000)
@@ -49,7 +72,7 @@ if not ds.has_free_space(percent_limit):
                     "Content-Type": "application/json",
                 },
                 data=json.dumps({
-                    "led": "yellow"
+                    "led": ds.has_free_space(percent_limit)	
                 })
             )
             print('Response HTTP Status Code: {status_code}'.format(
