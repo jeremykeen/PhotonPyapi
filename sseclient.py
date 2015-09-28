943ca6ad272f3d8588bef6aa448ee924d92e5232
Jeremy Keen <jeremykeen@gmail.com>
2015-09-28 13:18:21 -0400

    server-sent events python client script
    uses sseclient to retrieve published events in the SSE stream from
    particle

---
+++ sseclient.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import json
+from sseclient import SSEClient
+
+deviceID = '<deviceid>'
+accessToken = '<accesstoken>'
+sparkURL = 'https://api.spark.io/v1/devices/' + deviceID + '/events/?access_token=' + accessToken
+
+messages = SSEClient('https://api.particle.io/v1/devices/events?access_token=<deviceid>ß	')
+
+for msg in messages:
+    print 'Processing Particle Event: ', msg
+    outputMsg = msg.data
+    if type(outputMsg) is not str:
+        parsed_json = json.loads(outputMsg)
+        FilterName = "data"
+        print 'Parsed JSON data: '
+        print(parsed_json[FilterName])
